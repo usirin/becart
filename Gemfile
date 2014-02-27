e045c81d4a2017e304e8dfd0fb7c32bfbc0d65fd
Umut Sirin <umutsirin1@gmail.com>
2014-02-26 21:38:30 -0500

    create app

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bcrypt-ruby', '~> 3.1.2'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails', :require => false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Becart::Application.config.secret_key_base = '16a25c947e075abb102a3750b7e1f417ea98abe792bc837e22118a8883322b4fc2a8ac5de25362d7244cae0f62cd1aef692dca9769d5e97bb11e1ccd070891b3'
