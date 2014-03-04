9ea7bd85091248b3f2c26dd1a14da3baff27d629
usirin <umutsirin1@gmail.com>
2014-03-04 14:07:14 -0500

    Cached Link started

--- app/models/cached_link.rb
+++ app/models/cached_link.rb
@@ -1,2 +1,15 @@
 class CachedLink < ActiveRecord::Base
+  class NoLink < RuntimeError; end
+
+  attr_reader :url, :title, :description
+
+  def self.for_url(url)
+    db_link = find_by(:url => url) or raise NoLink
+    db_link.to_meta_link
+  end
+
+
+  def to_meta_link
+    LinkMeta.new
+  end
 end
--- spec/models/cached_link_spec.rb
+++ spec/models/cached_link_spec.rb
@@ -1,4 +1,23 @@
 require "spec_helper"
+require_relative "../../app/services/link_meta"
 
 describe CachedLink do
+  context "When there is a cached link in database" do
+    it "returns LinkMeta object" do
+      # cached_link = double(:url => "google.com", :description => "google rocks")
+      # CachedLink.stub(:find_by).with({:url => "google.com"}) { cached_link }
+      # expect do
+      #   CachedLink.for_url("google.com")
+      # end.to be_a LinkMeta
+    end
+  end
+
+  context "When there is not a cached link in database" do
+    it "throws an exception to say that" do
+      CachedLink.stub(:find_by) { nil }
+      expect do
+        CachedLink.for_url("google.com")
+      end.to
+    end
+  end
 end
